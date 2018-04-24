358955b2935dc0c4a5c96a66970d11985dd3d1b4
Greg Gómez <gogogo@unm.edu>
2018-04-24 02:16:06 +0000

    Fixes a referencial integrity problem. Tasks require a User.

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -9,14 +9,12 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class TaskTest extends TestCase
 {
     use DatabaseMigrations;
+    
     /** @test */
+    public function a_task_has_an_owner() {
+        $user = factory('App\User')->create();
+        $task = factory('App\Task')->create(['user_id' => $user->id]);
 
-    // This one needs to be fleshed out a little. :/
-    // I think it's not working because I didn't define the relationship
-    // somewhere. :\
-    //public function it_has_an_owner() {
-    //    $task = factory('App\Task')->create(['user_id'=>1]);
-
-    //    $this->assertInstanceOf('App\User', $task->owner);
-    //}
+        $this->assertInstanceOf('App\User', $task->owner);
+    }
 }
