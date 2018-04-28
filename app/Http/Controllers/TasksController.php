f2848975378b65e9272eb40ed8cc92c5eac93475
Greg Gómez <gogogo@unm.edu>
2018-04-28 14:33:52 +0000

    Renames to pluralize Tasks.

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Task;
 use Illuminate\Http\Request;
 
-class TaskController extends Controller
+class TasksController extends Controller
 {
     /**
      * Display a listing of the resource.
