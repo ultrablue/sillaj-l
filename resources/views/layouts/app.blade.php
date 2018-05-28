64f4a55a62f8766addd2094a8c7cf989aaf07c31
Greg Gómez <gogogo@unm.edu>
2018-05-28 22:48:31 +0000

    Adds Projects to the Navigation.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li><a href="projects">Projects</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
