d4472953688cbea0eae47df9cbbd17f45b2d936a
Greg Gómez <gogogo@unm.edu>
2018-05-28 22:52:24 +0000

    Oops fixes some styling for the nav.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li><a href="projects">Projects</a></li>
+                        <li class="nav-item"><a href="projects" class="nav-link">Projects</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
