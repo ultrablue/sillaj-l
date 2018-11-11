505cd34dc11bb6d3a11bfa2cd6c5d2cf00d5797b
Greg Gómez <gogogo@unm.edu>
2018-11-11 16:18:41 +0000

    Adds telescope.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,10 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
+                <div class="card-body">
+                    {{ today()->format('l, F jS, Y') }}
+                </div>
+                @include('navigation.calendar')
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success">
---
+++ resources/views/navigation/calendar.blade.php
@@ -0,0 +1,3 @@
+<div class='card-body'>
+<h1>Yo</h1>
+</div>
