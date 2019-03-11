61dc75a09f756560d7b95b5d2555ba39c63a1895
Greg Gómez <gogogo@unm.edu>
2019-03-11 20:49:07 +0000

    Updated to Boostrap 4.1.3.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,27 @@
 
     <div class="container" style="margin-top:30px">
 
+        <div class="row">
+            <div class="col-sm-12">
+                <p>
+                    <h2>Todo:</h2>
+                    <ul>
+                        <li>Complete the home route to get all events for the given date, and for today if no date given.</li>
+                        <li>Fix the form. It should be much more compact.</li>
+                        <li>Add the Events list</li>
+                        <li>Create the Calendar nav section</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+
+
+
         <div class="row"><!-- Top row -->
 
             <div class="col-sm-4" style="background:cornsilk;"><!-- Calendar Nav -->
-		Calendar Nav
-		@include('partials.calendar-nav')                
+                Calendar Nav
+                @include('partials.calendar-nav')                
             </div><!-- /Calendar Nav -->
 
 
