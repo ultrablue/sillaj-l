84568c38b5d7e8d5f711a12e6e35d514eaa7d261
Greg Gómez <gogogo@unm.edu>
2019-03-22 22:56:56 +0000

    Changes layout to use cards, adds some card-y look and feel

--- resources/views/partials/calendar-nav.blade.php
+++ resources/views/partials/calendar-nav.blade.php
@@ -1,29 +1,32 @@
 <!-- Calendar Navigation -->
-<div class="month">      
-    <ul>
-        <li class="prev">&#10094;</li>
-        <li class="next">&#10095;</li>
-        <li>{{ $month->title('Y-m F') }}</li>
-    </ul>
+<div class="card border-secondary">
+    <div class="card-body">
+        <div class="month">      
+            <ul>
+                <li class="prev">&#10094;</li>
+                <li class="next">&#10095;</li>
+                <li>{{ $month->title('Y-m F') }}</li>
+            </ul>
+        </div>
+
+        <ul class="weekdays">
+            @foreach ($month->weeks()[0]->days() as $day)
+                <li>{{ $day->date()->format('D') }}</li>
+            @endforeach
+        </ul>
+
+
+        <ul class="days">
+            @foreach ($month->weeks() as $week)
+                @foreach ($week->days() as $day)
+                    <li>{{ $day->date()->format('j') }}</li>
+                @endforeach
+            @endforeach
+        </ul>
+    </div>
 </div>
 
-<ul class="weekdays">
-    @foreach ($month->weeks()[0]->days() as $day)
-        <li>{{ $day->date()->format('D') }}</li>
-    @endforeach
-</ul>
-
-
-<ul class="days">
-    @foreach ($month->weeks() as $week)
-        @foreach ($week->days() as $day)
-            <li>{{ $day->date()->format('j') }}</li>
-        @endforeach
-    @endforeach
-</ul>
-
 <!-- End Calendar Navigation -->
-
 
 
 
