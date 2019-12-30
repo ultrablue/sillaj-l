744fd6c3b475f19d6d092b4360e775cd7ae12462
Greg Gomez <github_000001@incognitology.com>
2019-12-30 11:22:18 -0700

    Implements a partial for the form for both create and update.

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -17,62 +17,7 @@
 
     <h2>Create a new Project!</h2>
 
-    @if(isset ($project))
-        {!! Form::model($project, ['action' => ['ProjectsController@update']]) !!}
-    @else
-        {{ Form::open(['action' => 'ProjectsController@store']) }}
-    @endif
-    <div class="w-full px-3 mb-6">
-        {{Form::label('name', 'Name:', ['class' => 'block text-gray-700 text-sm']) }}
-        {{ Form::text('name',null,['class' => 'text-field focus:outline-none focus:shadow-outline']) }}
-    </div>
-
-    <div class="w-full px-3 mb-6">
-        {!! Form::label('description', 'Description:', ['class' => 'block text-gray-700 text-sm']) !!}
-        {!! Form::textarea('description',null,['class' => 'text-field focus:outline-none focus:shadow-outline', 'rows' => '3']) !!}
-    </div>
-
-    <div class="w-full px-3 mb-6">
-        {!! Form::label('display', 'Display:', ['class' => 'block text-gray-700 text-sm']) !!}
-        {{ Form::hidden('display', 0) }}
-        {!! Form::checkbox('display',1,null,null,['class' => 'text-field focus:outline-none focus:shadow-outline']) !!}
-    </div>
-
-    <div class="w-full px-3 mb-6">
-        {!! Form::label('share', 'Share:', ['class' => 'block text-gray-700 text-sm']) !!}
-        {{ Form::hidden('share', 0) }}
-        {!! Form::checkbox('share',1,null,null,['class' => 'text-field focus:outline-none focus:shadow-outline']) !!}
-    </div>
-
-    <div class="w-full px-3 mb-6">
-        {!! Form::label('use_in_reports', 'Use In Reports:', ['class' => 'block text-gray-700 text-sm']) !!}
-        {{ Form::hidden('use_in_reports', 0) }}
-        {!! Form::checkbox('use_in_reports',1,null,null,['class' => 'text-field focus:outline-none focus:shadow-outline']) !!}
-    </div>
-
-
-    <div class="w-full px-3 mb-6">
-
-        @foreach ($allTasks as $task)
-
-            @if(isset($project))
-                {{-- TODO This is probably horribly ineffecient!! --}}
-                @php $checked = in_array($task->id, $project->tasks->pluck('id')->toArray()) ? true : false @endphp
-                {{ Form::checkbox('tasks[]',$task->id) }} {{ $task->name }}<br>
-            @else
-                {{ Form::checkbox('tasks[]',$task->id) }} {{ $task->name }}<br>
-            @endif
-
-        @endforeach
-
-
-    </div>
-
-    {!! Form::submit('Save', ['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline']) !!}
-
-
-
-    {!! Form::close() !!}
+    @include('partials.project-from')
 
 @endsection
  
