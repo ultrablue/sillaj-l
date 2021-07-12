091fc84c24c71e32d6f5b1dd6bc53356a172e4f5
ultrablue <github_000001@incognitology.com>
2021-07-12 03:20:39 +0000

    ⚒ [WIP] - Adds a new column to hold the Delete link.

--- resources/views/partials/events-list.blade.php
+++ resources/views/partials/events-list.blade.php
@@ -11,26 +11,31 @@
             <div class="table-cell">Project</div>
             <div class="table-cell">Task</div>
             <div class="table-cell">Description</div>
+            <div class="table-cell">Delete</div>
         </div>
 
 
-        @foreach($thisDaysEvents as $event)
-            <div class="table-row" class="cursor-pointer" data-event-id="{{$event->id}}" onclick="window.location='{{ route('event-show', $event->id)}}';">
-                <!-- <div class="table-cell border-b-2 cursor-pointer">{{$event->id}}</div> -->
+        @foreach ($thisDaysEvents as $event)
+            <div class="table-row" class="cursor-pointer" data-event-id="{{ $event->id }}"
+                onclick="window.location='{{ route('event-show', $event->id) }}';">
+                <!-- <div class="table-cell border-b-2 cursor-pointer">{{ $event->id }}</div> -->
                 <div class="table-cell  border-b-2 cursor-pointer">
                     @isset($event->time_start)
-                        {{$event->start()->format('H:i')}}
+                        {{ $event->start()->format('H:i') }}
                     @endisset
                 </div>
                 <div class="table-cell border-b-2 cursor-pointer">
                     @isset($event->time_end)
-                        {{$event->end()->format('H:i')}}
+                        {{ $event->end()->format('H:i') }}
                     @endisset
                 </div>
-                <div class="table-cell  border-b-2 cursor-pointer">{{$event->iso_8601_duration->format('%H:%I')}}</div>
-                <div class="table-cell  border-b-2 cursor-pointer">{{$event->project->name ?? "No Project!"}}</div>
-                <div class="table-cell  border-b-2 cursor-pointer">{{$event->task->name ?? "No Task!"}}</div>
+                <div class="table-cell border-b-2 cursor-pointer">{{ $event->iso_8601_duration->format('%H:%I') }}
+                </div>
+                <div class="table-cell border-b-2 cursor-pointer">{{ $event->project->name ?? 'No Project!' }}</div>
+                <div class="table-cell border-b-2 cursor-pointer">{{ $event->task->name ?? 'No Task!' }}</div>
                 <div class="table-cell border-b-2 cursor-pointer">{{ str_limit($event->note, 30) }}</div>
+                <div class="table-cell border-b-2"><a href="#"><span class="oi text-red-700" data-glyph="delete"></a></span>
+                </div>
             </div>
 
         @endforeach
@@ -40,7 +45,8 @@
             <!-- <div class="table-cell">&nbsp;</div> -->
             <div class="table-cell"></div>
             <div class="table-cell"></div>
-            <div class="table-cell">{{$totalDuration->cascade()->format('%H:%I')}}</div>
+            <div class="table-cell">{{ $totalDuration->cascade()->format('%H:%I') }}</div>
+            <div class="table-cell"></div>
             <div class="table-cell"></div>
             <div class="table-cell"></div>
             <div class="table-cell"></div>
