017d52c29f0abd6ead7cc8ccacb65ca8964848c2
ultrablue <github_000001@incognitology.com>
2021-11-03 02:25:33 +0000

    Fixes a problem where multiple routes had the same name ('home').

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@
 
 Route::get('/home/', 'HomeController@index')->name('home');
 // where() constrains the route to the regex supplied.
-Route::get('/{eventdate?}', 'HomeController@index')->name('home')->where('eventdate', '\d\d\d\d-\d\d-\d\d');
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/{eventdate?}', 'HomeController@index')->where('eventdate', '\d\d\d\d-\d\d-\d\d');
+Route::get('/', 'HomeController@index');
 //Route::get('/', 'HomeController@index')->name('home');
 //Route::get('/home/', 'HomeController@index')->name('home');
 
