a0f5c04ff160fbb20550fcadcadd66d4191c85da
ultrablue <github_000001@incognitology.com>
2021-11-12 04:00:28 +0000

    Updates to new Factory.

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -1,27 +1,42 @@
 <?php
 
+namespace Database\Factories;
+
+
+
 // Projects Table Factory.
 
-use Faker\Generator as Faker;
+// use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-$factory->define(App\Project::class, function (Faker $faker) {
-    return [
-        //'user_id' => '', // This *must* be supplied by the caller!
-        'name'           => str_replace('.', '', $faker->text(20)),
-        'description'    => $faker->sentence(),
-        'display'        => TRUE,
-        'use_in_reports' => $faker->boolean(50),
-        'share'          => $faker->boolean(50)
-    ];
-});
+class ProjectFactory extends Factory
+{
 
-$factory->state(App\Project::class, 'firstProject', [
-    'name'           => 'sillaj-l Test Project',
-    'description'    => 'This is a static Test Project used for unit tests, etc.',
-    'display'        => TRUE,
-    'use_in_reports' => TRUE,
-    'share'          => TRUE
-]);
+    public function definition()
+    {
 
 
 
+        return [
+            //'user_id' => '', // This *must* be supplied by the caller!
+            'name'           => str_replace('.', '', $this->faker->text(20)),
+            'description'    => $this->faker->sentence(),
+            'display'        => TRUE,
+            'use_in_reports' => $this->faker->boolean(50),
+            'share'          => $this->faker->boolean(50)
+        ];
+    }
+
+    public function firstProject()
+    {
+        return $this->state(function (array $aatributes) {
+            return [
+                'name'           => 'sillaj-l Test Project',
+                'description'    => 'This is a static Test Project used for unit tests, etc.',
+                'display'        => TRUE,
+                'use_in_reports' => TRUE,
+                'share'          => TRUE
+            ];
+        });
+    }
+}
