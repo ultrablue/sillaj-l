acf177bf619ff9f9c52e78fa992e3a9808a8c73f
ultrablue <github_000001@incognitology.com>
2021-11-14 18:39:01 +0000

    Completes work on this file for now.

--- database/seeders/EventsTableSeeder.php
+++ database/seeders/EventsTableSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+use App\Project;
+use App\Event;
+use App\Task;
+
 class EventsTableSeeder extends Seeder
 {
     /**
@@ -14,20 +19,20 @@ class EventsTableSeeder extends Seeder
     public function run()
     {
         // Get all the Users from the database.
-        $users = App\User::all();
+        $users = User::all();
 
         // Seed Events.
         // TODO is there a way to do a callback rather than a closure?
         $users->each(function ($user) {
 
             // Get all Projects that are shared or that 'belong' the the User.
-            $projects = App\Project::where('user_id', '=', $user->id)
+            $projects = Project::where('user_id', '=', $user->id)
                 ->orWhere('share', '=', TRUE)
                 ->get();
 
             // Get each Task created by this User and create some Events associated with that Task.
             $user->tasks->each(function ($task) use ($user, $projects) {
-                factory('App\Event', 2)->create([
+                Event::factory()->count(2)->create([
                     'user_id'    => $user->id,
                     'task_id'    => $task->id,
                     // This probably shouldn't be random, but I'm not sure what to make it for now.
@@ -35,7 +40,7 @@ class EventsTableSeeder extends Seeder
                 ]);
 
                 // Create some Events with just durations, no start or end times.
-                factory('App\Event', 2)->states('duration')->create([
+                Event::factory()->count(2)->duration()->create([
                     'user_id'    => $user->id,
                     'task_id'    => $task->id,
                     'project_id' => $projects->random()->id
@@ -43,21 +48,18 @@ class EventsTableSeeder extends Seeder
             });
 
             // Get all of the shared Tasks that were NOT created by this User so that we can make some Events with those Tasks.
-            $sharedTasks = App\Task::where([
+            $sharedTasks = Task::where([
                 ['share', '=', '1'],
                 ['user_id', '<>', $user->id]
             ]);
 
             $sharedTasks->each(function ($task) use ($user, $projects) {
-               factory('App\Event', 2)->create([
+                Event::factory()->count(2)->create([
                     'user_id'    => $user->id,
                     'task_id'    => $task->id,
                     'project_id' => $projects->random()->id
                 ]);
-
             });
-
-
         });
     }
 }
