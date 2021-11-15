19bcc2ceab9fb4381a10643218098fdad7b444b8
ultrablue <github_000001@incognitology.com>
2021-11-15 01:41:19 +0000

    🚧 Partially converts to the new way.

--- tests/Feature/EventTest.php
+++ tests/Feature/EventTest.php
@@ -6,6 +6,10 @@ use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\User;
+use App\Project;
+use App\Task;
+use App\Event;
 
 class EventTest extends TestCase
 {
@@ -15,7 +19,7 @@ class EventTest extends TestCase
     /** @test */
     public function an_authenticated_user_with_no_events_for_a_given_date_sees_no_events_in_the_dashboard()
     {
-        $user = factory('App\User')->create();
+        $user = User::factory()->create();
         $response = $this->actingAs($user)->get('/');
         $response->assertSee('You don\'t have any Events, yet.', false);
     }
@@ -26,14 +30,14 @@ class EventTest extends TestCase
     public function an_authenticated_user_can_view_all_events_for_a_given_date()
     {
         // Given an authenticated User;
-        $user = factory('App\User')->create();
+        $user = User::factory()->create();
         // and a date;
         $date = Carbon::now();
         // the User can see Events for that Date.
-        $firstProject = factory('App\Project')->create(['user_id' => $user->id]);
-        $firstTask = factory('App\Task')->create(['user_id' => $user->id]);
+        $firstProject = Project::factory()->create(['user_id' => $user->id]);
+        $firstTask = Task::factory()->create(['user_id' => $user->id]);
 
-        $firstEvent = factory('App\Event')->create(['user_id' => $user->id, 'project_id' => $firstProject->id, 'task_id' => $firstTask->id, 'event_date' => $date]);
+        $firstEvent = Event::factory()->create(['user_id' => $user->id, 'project_id' => $firstProject->id, 'task_id' => $firstTask->id, 'event_date' => $date]);
         $response = $this->actingAs($user)->get('/');
         $response->assertSee(str_limit($firstEvent->note, 30), false);
     }
@@ -44,7 +48,7 @@ class EventTest extends TestCase
     public function an_authenticated_user_can_view_new_the_event_form()
     {
         // Given a User...
-        $user = factory('App\User')->create();
+        $user = User::factory()->create();
         // ...that User can see the Event Form.
         $response = $this->actingAs($user)->get('/');
         $response->assertSee('Create or Edit an Event', false);
@@ -56,7 +60,7 @@ class EventTest extends TestCase
     public function an_authenticated_user_can_create_a_new_task_with_a_start_and_end_date()
     {
         // Given a User...
-        $user = factory('App\User')->create();
+        $user = User::factory()->create();
         // ... a Date
         $date = Carbon::now();
     }
