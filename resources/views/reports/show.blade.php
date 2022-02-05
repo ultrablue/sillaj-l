1474f58f0034a3b0e3b313a429a11e6906f93c3b
ultrablue <github_000001@incognitology.com>
2022-02-05 18:31:05 +0000

    Makes the report show Tasks by Project and Projects by Task, but probably makes the code more confusing. 😕

--- resources/views/reports/show.blade.php
+++ resources/views/reports/show.blade.php
@@ -15,25 +15,25 @@
                 $currentProject = null;
             @endphp
             @foreach ($events as $i => $row)
-                @if ($currentProject !== $row->project && $row->project != null)
+                @if ($currentProject !== $row[strtolower($group[0])] && $row[strtolower($group[0])] != null)
                     @php
-                        $currentProject = $row->project;
+                        $currentProject = $row[strtolower($group[0])];
                     @endphp
                     <tr class="border-b-2 border-gray-500">
-                        <th colspan="3" class="pt-5 text-left">Project: {{ $row->project }}</th>
+                        <th colspan="3" class="pt-5 text-left">{{ $group[0] }}: {{ $row[strtolower($group[0])] }}</th>
                     </tr>
                 @endif
 
 
-                @if ($row->task && $row->project)
-                    {{-- Task Row --}}
+                @if ($row[strtolower($group[0])] && $row[strtolower($group[1])])
+                    {{-- Level 2 Row --}}
                     <tr>
-                        <td colspan="1" class="w-1/2 pl-5"><span class="">{{ $row->task }}</span></td>
+                        <td colspan="1" class="w-1/2 pl-5"><span class="">{{ $row[strtolower($group[1])] }}</span></td>
                         <td class="text-right">{{ sprintf('%.2f',round(Carbon\CarbonInterval::seconds($row->duration)->cascade()->total('hours'),2)) }}</td>
                         {{-- {{ Carbon\CarbonInterval::seconds($row->duration)->cascade()->format('%h:%I') }} --}}
                         <td class="pl-5 text-right">{{ sprintf('%.2f', round(100 * ($row->duration / $events->last()->duration), 2)) }}%</td>
                     </tr>
-                @elseif (!$row->project && !$row->task)
+                @elseif (!$row[strtolower($group[0])] && !$row[strtolower($group[1])])
                     {{-- Grand Total --}}
                     <tr class="bg-blue-300 border-t border-blue-900 text-2xl">
                         <td colspan="1">Grand Total</td>
@@ -41,7 +41,7 @@
                         {{-- {{ Carbon\CarbonInterval::seconds($row->duration)->cascade()->format('%h:%I') }} --}}
                         <td></td>
                     </tr>
-                @elseif ($row->project && !$row->task)
+                @elseif ($row[strtolower($group[0])] && !$row[strtolower($group[1])])
                     {{-- Level 1 Total --}}
                     <tr class="bg-blue-200 pb-5 font-bold">
                         <td colspan="1" class="pl-5">{{ $currentProject }} Total</td>
