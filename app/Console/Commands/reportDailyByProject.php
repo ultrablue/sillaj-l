163df79b0a9efa03913f860789d1b1c74370004d
ultrablue <github_000001@incognitology.com>
2022-02-07 03:27:54 +0000

    🚧Partial implementation of new reports that use ROLLed UP results.

--- app/Console/Commands/reportDailyByProject.php
+++ app/Console/Commands/reportDailyByProject.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use App\Mail\Report;
 use App\User;
+use App\Event;
+use Carbon\CarbonImmutable;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
@@ -43,32 +45,43 @@ class ReportDailyByProject extends Command
      */
     public function handle()
     {
-        $this->info($this->signature.' ran!');
+        // $this->info($this->signature . ' ran!');
 
         $allUsers = User::all();
         foreach ($allUsers as $user) {
             // TODO Make this a CarbonImmutable, please.
-            $now = now();
+            // $now = now();
 
             // TODO Let's move this to the User Model, please.
-            $eventsCollection = $user->events()
-            ->whereBetween('event_date', [$now->toDateString(), $now->toDateString()])
-            ->with(['task', 'project'])
-            ->get();
+            // $eventsCollection = $user->events()
+            // ->whereBetween('event_date', [$now->toDateString(), $now->toDateString()])
+            // ->with(['task', 'project'])
+            // ->get();
 
-            $this->info($user->name.': '.$eventsCollection->count());
+
+            $startTime = $endTime = new CarbonImmutable();
+            // TODO Rather than used $user->id here, can we set the application's currently logged in user to $user? That way we wouldn't need a special method.
+            //      This doesn't appear to work: Auth::loginUsingId($user->id, true);
+            $eventsCollection = Event::rollupByProjectForUser($startTime, $endTime, $user);
+
+            // dd($eventsCollection->count());
+
+
+            $this->info($user->name . ' (id ' . $user->id . '): ' . $eventsCollection->count());
+
 
             if ($eventsCollection->count() === 0) {
-                Log::info('No daily report sent to user Id '.$user->id.': 0 Events.');
+                Log::info('No daily report sent to user Id ' . $user->id . ': 0 Events.');
                 continue;
             }
 
+            exit;
             $totalTime = $eventsCollection->sum('duration');
 
             // By Project, then Task.
             $eventsCollection = $eventsCollection->sortBy(['project.name', 'task.name'])->groupBy(['project.name', 'task.name']);
             $groupDisplayArray = ['Project', 'Task'];
-            $reportHeader = 'Hours for '.$now->format('l F j, Y');
+            $reportHeader = 'Hours for ' . $now->format('l F j, Y');
 
             Mail::to($user)->send(new Report($eventsCollection, $groupDisplayArray, $totalTime, $now, $reportHeader));
         }
