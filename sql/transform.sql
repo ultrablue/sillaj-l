cae7e8f1b80d65aef60016914f9247731090fe0a
ultrablue <github_000001@incognitology.com>
2022-03-07 03:15:37 +0000

    Updates.

--- sql/transform.sql
+++ sql/transform.sql
@@ -2,18 +2,12 @@
     Converts original Sillaj data to sillaj-l.
     Note the User Id in the WHERE clause!
 */
-
-SELECT
+CREATE TABLE `ultraspa_000-sillaj-l-events-test`.test AS SELECT
     sillaj_task_intTaskId AS task_id,
     sillaj_project_intProjectId AS project_id,
     '1' AS user_id,
     timStart AS time_start,
     timEnd AS time_end,
-    datEvent AS event_date,
-    strRem AS note,
-    datUpdate AS created_at,
-    datUpdate AS updated_at,
-    NULL AS deleted_at,
     (
         (
             (HOUR(timDuration) * 60) + MINUTE(timDuration)
@@ -25,8 +19,13 @@ SELECT
         'H',
         MINUTE(timDuration),
         'M'
-    ) AS iso_8601_duration
+    ) AS iso_8601_duration,
+    datEvent AS event_date,
+    strRem AS note,
+    datUpdate AS created_at,
+    datUpdate AS updated_at,
+    NULL AS deleted_at
 FROM
     `sillaj_event`
 WHERE
-    sillaj_event.sillaj_user_strUserId = 'gogogo'
+    sillaj_event.sillaj_user_strUserId = 'gogogo';
