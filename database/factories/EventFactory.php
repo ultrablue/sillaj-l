781a3bbe67b9d37ff0c4f04b492b1de2a71681f5
ultrablue <github_000001@incognitology.com>
2023-07-16 16:36:49 +0000

    🧑‍💻 Makes the resulting events a little more realistic and readable. The original code would create times like: 08:17:23. Now it rounds those to the nearest 5 minutes, 08:20:00. In addition, durations were in a similar state. For example a duration might be 1:32. Now it wold be 1:30.

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -11,19 +11,31 @@ class EventFactory extends Factory
 {
     public function definition()
     {
-        // $factory->define(App\Event::class, function (Faker $this->faker) {
+        // I think the idea here is to select a random date within the next two weeks.
         $startDate = Carbon::createFromTimeStamp($this->faker->dateTimeThisMonth('+2 week')->getTimeStamp());
-        $endDate = $startDate->copy()->addMinutes($this->faker->numberBetween(5, 180));
+        // This bit sets the time to a multiple of 5 minutes.
+        $resolution = 5;
+        $s = $resolution * 60;
+        $startDate->setTimestamp($s * (int) ceil($startDate->getTimestamp() / $s));
+        // The idea here is to keep the durations to increments of 5 minutes. This isn't strictly necessary, 
+        // but it makes the data easier to check visually. And it's more realstic. I don't think I've ever
+        // made an Event with any time that wasn't rounded to the nearest 5 minutes.
+        // In this case, the minimum and maximum will be:
+        //    - 3 * 5 = 15
+        //    - 48 * 5 = 240 minutes = 4 hours
+        $eventDuration = $this->faker->numberBetween(3, 40) * 5;
+        $endDate = $startDate->copy()->addMinutes($eventDuration);
+        // TODO this is redundant, no?
         $duration = $endDate->diffAsCarbonInterval($startDate);
 
         return [
-        'user_id' => '',
-        'time_start' => $startDate->toTimeString(),
-        'time_end' => $endDate->toTimeString(),
-        'duration' => $duration->spec(),
-        'event_date' => $startDate->toDateString(),
-        'note' => $this->faker->paragraph(3, true),
-    ];
+            'user_id' => '',
+            'time_start' => $startDate->toTimeString(),
+            'time_end' => $endDate->toTimeString(),
+            'duration' => $duration->spec(),
+            'event_date' => $startDate->toDateString(),
+            'note' => $this->faker->paragraph(3, true),
+        ];
     }
 
     public function duration()
@@ -31,14 +43,18 @@ class EventFactory extends Factory
         // return $this->state(App\Event::class, 'duration', function (Faker $this->faker) {
 
         return $this->state(function (array $attributes) {
-            $duration = CarbonInterval::create(0, 0, 0, 0, $this->faker->numberBetween(0, 7), $this->faker->numberBetween(1, 59), 0, 0);
+            // See above for an explanation.
+            $eventDuration = $this->faker->numberBetween(3, 40) * 5;
+
+            // years, months, weeks, days, hours, minutes, seconds, microseconds
+            $duration = CarbonInterval::create(0, 0, 0, 0, 0, $eventDuration, 0, 0);
 
             return
-            [
-                'duration' => $duration->spec(),
-                'time_start' => null,
-                'time_end' => null,
-            ];
+                [
+                    'duration' => $duration->spec(),
+                    'time_start' => null,
+                    'time_end' => null,
+                ];
         });
     }
 }
