745eab99fe09b23394e8df033526f5027ca2adf9
ultrablue <github_000001@incognitology.com>
2023-08-06 15:40:56 +0000

    Partial implementation of styling the report itself.

--- resources/views/reports/newReportTest.blade.php
+++ resources/views/reports/newReportTest.blade.php
@@ -3,31 +3,31 @@
 @section('content')
    <div class="mt-20">
 
+      <h1 class="text-4xl mb-8">Report</h1>
+
       <h1 class="text-2xl">{{ ucfirst($group[1]) }} by {{ ucfirst($group[0]) }}</h1>
       <h2>{{ $dates['start-date']->format('l, F jS, Y') }} through {{ $dates['end-date']->format('l, F jS, Y') }}</h2>
 
 
 
-      <h1 class="text-4xl mb-8">Report</h1>
-      <h1>Where you're at: You need to make the controller handle a POST, not a GET. So, inject some POSTed values.</h1>
 
-      {{-- @dd($groupedEvents) --}}
-      {{-- @dd($projectTotals['Dolore est iste']->total_duration) --}}
-
+      {{-- @dump($group) --}}
       <div>
-         @foreach ($groupedEvents as $project => $tasks)
-            <div>
-               Project: {{ $project }}
+         @foreach ($groupedEvents as $firstLevel => $secondLevel)
+            <div class="mt-10 font-bold text-2xl font-mono">
+               {{ ucfirst($group[0]) }}: {{ $firstLevel }}
+            </div>
+            <div class="pl-2 italic">
+               <span class="font-semibold">{{ $firstLevel }}</span> all-time duration: {{ secondsToHm($projectTotals[$firstLevel]->total_duration) }}
             </div>
 
             <div class="mb-10">
-               @foreach ($tasks as $taskName => $events)
-                  <div>Task: {{ $taskName }}</div>
-                  <div>Sum: {{ $events->sum('duration') }}</div>
-                  <div>Percentage: {{ ($events->sum('duration') / $ungroupedEvents->sum('duration')) * 100 }}</div>
+               @foreach ($secondLevel as $secondLevelName => $events)
+                  <div class="pl-8 bg-orange-300">{{ ucfirst($group[1]) }}: {{ $secondLevelName }}</div>
+                  {{-- <div>Sum: {{ $events->sum('duration') }}</div> --}}
+                  <div>Percentage: {{ round(($events->sum('duration') / $ungroupedEvents->sum('duration')) * 100) }}%</div>
                   <div>Duration (decimal): {{ sprintf('%.2f',round(Carbon\CarbonInterval::seconds($events->sum('duration'))->cascade()->total('hours'),2)) }}</div>
                   <div>Duration (hh:mm): {{ secondstoHm($events->sum('duration')) }}</div>
-                  <div>Project All Time Total Duration: {{ secondsToHm($projectTotals[$project]->total_duration) }} ({{ $projectTotals[$project]->total_duration }})</div>
                   <hr>
                @endforeach
             </div>
